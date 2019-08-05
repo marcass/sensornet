d2248c1c9c16e3db2c79facc75f8592842d34cc8
Marcass <best.nose@gmail.com>
2019-08-05 22:34:47 +1200

    testing stuff

--- sensors-frontend/src/utils/api.js
+++ sensors-frontend/src/utils/api.js
@@ -6,8 +6,8 @@ import Router from 'vue-router'
 
 Vue.use(VueAxios, axios, Router)
 
-const BASE_URL = 'http://localhost:8001/kong';
-// const BASE_URL = 'https://skibo.duckdns.org/kong';
+// const BASE_URL = 'http://localhost:8001/kong';
+const BASE_URL = 'https://skibo.duckdns.org/api';
 // const BASE_URL = 'http://nuc/kong';
 Vue.axios.defaults.baseURL = BASE_URL;
 // axios.defaults.headers.common['Authorization'] = 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqMXVnc3hQYll6Nldnb0NtWEtmMHpxb3kwU1NySTdHYSIsIm5iZiI6MTU1MTIxMTYwNywic3ViIjp7InVzZXJuYW1lIjoiYW5keSJ9LCJpYXQiOjE1NTEyMTE2MDcsImV4cCI6MTU1MTIxNTIwNywicm9sZXMiOlsicGxlYiIsImZ1Y2tlciJdfQ.8iRuEICHnueIFrTFwmS2U3y8h4QVsXfIjGg7GhwFy9Q';
@@ -141,7 +141,8 @@ function storeToken (result) {
   // console.log(result)
   var tokenData = result.data
   var decoded = jwt_decode(tokenData.access_token)
-  // console.log(decoded)
+  console.log(decoded)
+  console.log(tokenData.access_token)
   // set items in storage
   // localStorage.setItem('username', user.username)
   localStorage.setItem('roles', decoded.roles)
@@ -180,7 +181,7 @@ function simple_get(url) {
 
 function testGet() {
   authHeader()
-  const url = BASE_URL+'/test/flask/hello'
+  const url = BASE_URL+'/data/values/types'
   return simple_get(url)
 }
 
@@ -209,5 +210,5 @@ function testGet() {
 
 function testLogin (user) {
   // console.log(user)
-  return axios.post(BASE_URL+'/auth', user)
+  return axios.post(BASE_URL+'/auth/login', user)
 }
